2bc77960cb052a4520405d34076c6ebeccfd9a90
Tyler McCoy <144307884+tjmccoy@users.noreply.github.com>
2025-02-18 22:44:02 -0600

    Add title screen predicate (#40)
    * Add title screen predicate
    
    * Actually added it this time whoops

---
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/predicate/TitleScreenPredicate.kt
@@ -0,0 +1,22 @@
+package liltojustice.trueadaptivemusic.client.predicate
+
+import com.google.gson.JsonObject
+import net.minecraft.client.MinecraftClient
+
+class TitleScreenPredicate(partialPath: String)
+    : MusicPredicate(partialPath) {
+
+    override fun test(client: MinecraftClient): Boolean {
+        return client.world == null;
+    }
+
+    override fun getIDs(): List<String> { return emptyList() }  // return immutable list, won't be using this
+
+    companion object: MusicPredicateCompanion<TitleScreenPredicate> {
+        override fun getTypeName(): String { return "title_screen" }
+
+        override fun fromJson(json: JsonObject, partialPath: String): TitleScreenPredicate {
+            return TitleScreenPredicate(partialPath)
+        }
+    }
+}
