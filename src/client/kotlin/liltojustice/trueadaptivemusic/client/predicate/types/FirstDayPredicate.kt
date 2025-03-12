f253630a2e85c7019f6b0b0f3515d15559553c99
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2025-03-11 22:16:42 -0700

    Add FirstDayPredicate (#106)

---
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/predicate/types/FirstDayPredicate.kt
@@ -0,0 +1,20 @@
+package liltojustice.trueadaptivemusic.client.predicate.types
+
+import com.google.gson.JsonObject
+import net.minecraft.client.MinecraftClient
+
+class FirstDayPredicate(): MusicPredicate() {
+    override fun test(client: MinecraftClient): Boolean {
+        val time = client.world?.time ?: return false
+
+        return time <= 24000L
+    }
+
+    companion object: MusicPredicateCompanion<FirstDayPredicate> {
+        override fun getTypeName(): String { return "first_day" }
+
+        override fun fromJson(json: JsonObject): FirstDayPredicate {
+            return FirstDayPredicate()
+        }
+    }
+}
