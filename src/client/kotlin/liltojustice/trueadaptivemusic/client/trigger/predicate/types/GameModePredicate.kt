bdae11811fecd74564838b1f0800b1f981ba6d74
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2025-07-29 22:50:33 -0700

    Added GameModePredicate.kt (#185)

---
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/GameModePredicate.kt
@@ -0,0 +1,32 @@
+package liltojustice.trueadaptivemusic.client.trigger.predicate.types
+
+import com.google.gson.JsonObject
+import liltojustice.trueadaptivemusic.client.trigger.predicate.MusicPredicate
+import net.minecraft.client.MinecraftClient
+import net.minecraft.util.JsonHelper
+import net.minecraft.world.GameMode
+
+class GameModePredicate(private val gameMode: GameMode): MusicPredicate() {
+    override fun test(client: MinecraftClient): Boolean {
+        val currentGameMode = client.networkHandler?.getPlayerListEntry(client.player?.uuid ?: return false)?.gameMode
+
+        return currentGameMode == gameMode
+    }
+
+    override fun toJson(): JsonObject {
+        val result = super.toJson()
+        result.addProperty(FIELD_NAME, gameMode.name)
+
+        return result
+    }
+
+    companion object: MusicPredicateCompanion<GameModePredicate> {
+        override fun getTypeName(): String { return "game_mode" }
+
+        override fun fromJson(json: JsonObject): GameModePredicate {
+            return GameModePredicate(GameMode.valueOf(JsonHelper.getString(json, FIELD_NAME)))
+        }
+
+        private const val FIELD_NAME = "gameMode"
+    }
+}
